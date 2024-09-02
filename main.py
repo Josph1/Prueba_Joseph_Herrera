a2441a2cc105c551e7dc10ace6af42ff4eb7308e
Josph1 <48578479+Josph1@users.noreply.github.com>
2024-09-02 14:01:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+def encontar_el_subarreglo(arreglo_inicial, numero_objetivo):
+  n = len(arreglo_inicial)
+  max_tamaño = 0
+  arreglo_maximo = []
 
+  for _ in range(n):
+    suma_actual = 0
+    for j in range(_,n)
+      suma += arreglo_inicial[j]
+      if suma == numero_objetivo and (j - i + 1) > max_tamaño:
+        max_tamaño = j - i + 1
+        arregla_maximo = arreglo_inicial[i:j+1]
+
+  return arreglo_maximo
+        
+    
