9f04f6b30c0d39d6a6743cbdf8483ae8e519b56d
Josph1 <48578479+Josph1@users.noreply.github.com>
2024-09-02 14:23:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from flask import Flask, requests , jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/subarreglo', methods=['POST'])
+
+def encontrar_subarreglo():
+  datos = request.get_json()
+  arreglo = data.get('array')
+  numero_objetivo = datos.get('target')
+
+subarreglo = encontrar
+
+
 
